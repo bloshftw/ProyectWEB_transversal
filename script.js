1d2e94c18b80e920cdc651692815a2ba840e6d98
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2024-12-02 06:25:10 -0300

    Commit
    Se agregaron imagenes nuevas, se optimizaron el peso de las mismas

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
-        './assets/imagenes-carrusel/2.jpg',
         './assets/imagenes-carrusel/1.jpg',
+        './assets/imagenes-carrusel/2.jpg',
         './assets/imagenes-carrusel/3.jpg',
-        './assets/imagenes-carrusel/6.jpg',
-        './assets/imagenes-carrusel/8.jpg',
-        './assets/imagenes-carrusel/11.jpg',
-        './assets/imagenes-carrusel/12.jpg',
-        './assets/imagenes-carrusel/105.jpg'
+        './assets/imagenes-carrusel/4.jpg',
+        './assets/imagenes-carrusel/5.jpg'
     ];
 
     let currentIndex = 0;
@@ -79,78 +76,3 @@ menuHamburguesa.addEventListener('click', () => {
     navMenu.classList.toggle('show'); // Alterna la clase "show"
 });
 
-/*FORMULARIO INICIO*/
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    let isValid = true;
-
-    // Limpiar msjes
-    document.getElementById("nameError").textContent = "";
-    document.getElementById("emailError").textContent = "";
-    document.getElementById("phoneError").textContent = "";
-    document.getElementById("messageError").textContent = "";
-    document.getElementById("successMessage").textContent = "";
-
-    // Validar nombres minimo 3 caract
-    if (name.length < 3) {
-        document.getElementById("nameError").textContent = "El nombre debe tener al menos 3 caracteres.";
-        isValid = false;
-    }
-
-    // expresion regular validar
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(email)) {
-        document.getElementById("emailError").textContent = "El correo electrónico no es válido.";
-        isValid = false;
-    }
-
-    // validar nro telefono
-    const phonePattern = /^[0-9]{10}$/; // 10 digitos
-    if (!phonePattern.test(phone)) {
-        document.getElementById("phoneError").textContent = "El teléfono debe contener solo números (10 dígitos).";
-        isValid = false;
-    }
-
-    // Validar mensajes
-    if (message.length === 0) {
-        document.getElementById("messageError").textContent = "El mensaje no puede estar vacío.";
-        isValid = false;
-    } else if (message.length > 300) {
-        document.getElementById("messageError").textContent = "El mensaje no debe superar los 300 caracteres.";
-        isValid = false;
-    }
-
-    // mostrar datos si son validos
-    if (isValid) {
-        const successMessage = document.getElementById("successMessage");
-        successMessage.textContent = "Datos enviados correctamente:";
-
-        // Crear elementos para mostrar los datos
-        const nameElement = document.createElement("p");
-        nameElement.textContent = "Nombre: " + name;
-
-        const emailElement = document.createElement("p");
-        emailElement.textContent = "Email: " + email;
-
-        const phoneElement = document.createElement("p");
-        phoneElement.textContent = "Teléfono: " + phone;
-
-        const messageElement = document.createElement("p");
-        messageElement.textContent = "Mensaje: " + message;
-
-        // agregar elementos al dom
-        successMessage.appendChild(nameElement);
-        successMessage.appendChild(emailElement);
-        successMessage.appendChild(phoneElement);
-        successMessage.appendChild(messageElement);
-
-        // Limpiar el formulario
-        document.getElementById("contactForm").reset();
-    }
-});
-/*FORMULARIO FINAL*/ 
